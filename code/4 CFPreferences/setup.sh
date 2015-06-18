d378d8aca1a018556975ef3b7d60a72b51287a20
Timothy Sutton <tim@synthist.net>
2015-06-18 08:54:03 -0600

    Minor changes

--- code/4 CFPreferences/setup.sh
+++ code/4 CFPreferences/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-
+# clearing some app prefs for demonstrating CFPreferences from within Python
 defaults write com.apple.Safari NewTabBehavior -integer 0
 defaults write com.apple.Safari NewWindowBehavior -integer 0
 defaults delete com.apple.Safari HomePage
+
+defaults delete com.adobe.Reader
